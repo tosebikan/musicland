6dd6c7f1d9a9a990fcbdfa29b9924477a6a98aca
tosebikan <tosebikan@gmail.com>
2021-02-01 22:21:14 +0000

    add assets, & theme

---
+++ constants/icons.js
@@ -0,0 +1,17 @@
+export const back = require('../icons/back_icon');
+export const cart = require('../icons/cart_icon');
+export const dashboard = require('../icons/dashboard_icon');
+export const menu = require('../icons/menu_icon');
+export const plus = require('../icons/plus_icon');
+export const search = require('../icons/search_icon');
+export const user = require('../icons/user_icon');
+
+export default {
+  back,
+  cart,
+  dashboard,
+  menu,
+  plus,
+  search,
+  user,
+};
---
+++ constants/images.js
@@ -0,0 +1,13 @@
+export const guiter1 = require('../images/guiter1');
+export const guiter2 = require('../images/guiter2');
+export const piano = require('../images/piano');
+export const trumpet = require('../images/trumpet');
+export const violin = require('../images/violin');
+
+export default {
+  guiter1,
+  guiter2,
+  piano,
+  trumpet,
+  violin,
+};
